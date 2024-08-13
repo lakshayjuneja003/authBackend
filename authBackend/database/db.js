cdcd05c84a1a6c264f19b8cbaef5284ba28cafed
lakshayjuneja003 <lakshayjuneja003@gmail.com>
2024-08-13 08:51:58 -0400

    updated db url

--- authBackend/database/db.js
+++ authBackend/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async ()=>{
     try {
-        const connectionInstance = await mongoose.connect("mongodb+srv://lakshayjuneja003:lakshayjuneja003@cluster0.djmep22.mongodb.net/user")
+        const connectionInstance = await mongoose.connect("mongodb+srv://username:passoword@cluster0.djmep22.mongodb.net/user")
         console.log(`\\Db connected to ${connectionInstance.Connection}`);
         
     } catch (error) {
